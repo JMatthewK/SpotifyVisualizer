79b58218f07a323c3c576501b15c466fd5a86f5a
John Matthew Kassapian <matthewkassapian4@gmail.com>
2024-09-17 13:32:14 -0400

    Revision 3 - Adding scene classes, fixing errors

--- src/homepage.java
+++ src/homepage.java
@@ -1,8 +1,24 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class homepage {
+    private Main main;
+    private StackPane pane;
 
+    public homepage(Main main){
+        this.main = main;
+        pane = new StackPane();
+
+        Button button = new Button("Go to Scene 2");
+        //button.setOnAction(e -> main.showScene2());
+
+        pane.getChildren().add(button);
+    }
+
+    public StackPane getPane() {
+        return pane;
+    }
 }
